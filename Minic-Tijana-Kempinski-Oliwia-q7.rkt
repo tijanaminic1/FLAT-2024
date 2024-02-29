43148753e23b45e53cc5aabfec4ab483c923463c
oliwial23 <oliwia1406@gmail.com>
2024-02-29 14:28:28 -0500

    setting up file

---
+++ Minic-Tijana-Kempinski-Oliwia-q7.rkt
@@ -0,0 +1,15 @@
+#lang fsm
+;; function that takes as input ndfa and returns one with only one final state
+
+#| Proof that any ndfa may be implemented with a single final state.
+
+ABC
+
+F
+
+trans to ABC
+
+Empty from each to ABC
+
+
+|#
